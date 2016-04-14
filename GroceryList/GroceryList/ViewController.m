4efa103f87ada90dca16c6bd5204ce3f32b60974
AustinRClark <aoclark@mix.wvu.edu>
2016-04-14 11:01:49 -0400

    finished. forgot to copy and redo the push to my account and not the wvu-ric account.

--- GroceryList/GroceryList/ViewController.m
+++ GroceryList/GroceryList/ViewController.m
@@ -18,33 +18,40 @@
     [super viewDidLoad];
     
     // TODO: Clear list when the app loads
-    
+    _itemListTextView.text = @"";
     // TODO: Initialize itemList array property
-    
+    _itemList = [[NSMutableArray alloc] init];
 }
 
 -(void)clearList{
     // TODO: Clear the text of the itemList property
-
+    _itemListTextView.text = @"";
+    
 }
 
 -(void)refreshList{
     
     // TODO: Clear the list
-
+    [self clearList];
     
     // TODO: Print out all of the items and their prices
-
+    if ([_itemList count] !=0){
+        NSString * newItemText = [[NSString alloc] init];
+        for (Item *item in _itemList) {
+            newItemText = [newItemText stringByAppendingString:[item.description stringByAppendingString: @"\n"]];
+        }
+         _itemListTextView.text = newItemText;
+    }
     
     // TODO: Set the total label
-
+    [self setTotalLabel];
 }
 
 - (void)setTotalLabel{
     float sum = 0.00;
     for (Item *item in self.itemList) {
         // TODO: Get the float value of item.price and add it to sum
-        
+        sum += [item.price floatValue];
     }
     
     
@@ -69,29 +76,34 @@
     toInsert.price = [nf numberFromString:self.itemPrice.text];
     
     // TODO: Add the item to the top of the list
-
+    
+    [self.itemList insertObject:toInsert atIndex:0];
     
     // TODO: Clear text fields
-
+    self.itemName.text = @"";
+    self.itemPrice.text = @"";
     
     // TODO: Refresh list
-    
+    [self refreshList];
     // TODO: Dismiss keyboard
-
+    [self.view endEditing:YES];
 }
 
 - (IBAction)removeLastItemTapped:(id)sender {
     
     // TODO: Remove first item in the self.itemList
-    
+    if([_itemList count] !=0)
+        [self.itemList removeObjectAtIndex:0];
     // TODO: Refresh list
-    
+    [self refreshList];
 }
 
 - (IBAction)clearListTapped:(id)sender {
     // TODO: Remove all items from self.itemList
-    
+    [self clearList];
+    if([_itemList count] !=0)
+        [self.itemList removeAllObjects];
     // TODO: Refresh the list
-    
+    [self refreshList];
 }
 @end
